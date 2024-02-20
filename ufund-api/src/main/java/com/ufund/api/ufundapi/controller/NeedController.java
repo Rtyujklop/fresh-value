c09e4cc5583c264ec7dbe930d6d1e67e705a6ed9
Ryan Noyes <rgn1061@rit.edu>
2024-02-20 14:14:45 -0500

    finished early need controller, needs testing/commenting

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/NeedController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/NeedController.java
@@ -19,6 +19,114 @@ import java.util.logging.Logger;
 import com.ufund.api.ufundapi.persistence.NeedDAO;
 import com.ufund.api.ufundapi.model.Need;
 
-public class NeedController {
-    
+@RestController
+@RequestMapping("needs")
+public class NeedController 
+{
+    public NeedController(NeedDAO needDao)
+    {   
+        this.needDao = needDao;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Need> getNeed(@PathVariable int id) {
+        LOG.info("GET /needs/" + id);
+        try {
+
+            Need need = needDao.getNeed(id);
+
+            if (need != null)
+                return new ResponseEntity<Need>(need,HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) 
+        {
+
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        
+        }
+    }
+
+
+    @GetMapping("")
+    public ResponseEntity<Need[]> getNeeds() {
+        LOG.info("GET /needs");
+        try 
+        {
+            Need[] needs = needDao.getNeeds();
+            return new ResponseEntity<Need[]>(needs, HttpStatus.OK);
+        } 
+        catch(IOException e)
+        {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Need[]> searchNeeds(@RequestParam String name) {
+        LOG.info("GET /needs/?name="+name);
+        try
+        {
+            Need[] needs = needDao.findNeeds(name);
+
+            return new ResponseEntity<Need[]>(needs, HttpStatus.OK);
+        }
+        catch(IOException e)
+        {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Need> createNeed(@RequestBody Need need) {
+        LOG.info("POST /needs " + need);
+        try 
+        {
+            Need createNeed = needDao.createNeed(need);
+            return new ResponseEntity<Need>(createNeed, HttpStatus.CREATED);
+        }
+        catch(IOException e)
+        {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Need> updateNeed(@RequestBody Need need) {
+        LOG.info("PUT /needs " + need);
+        try {
+            Need updateNeed = needDao.getNeed(need.getId());
+            if (updateNeed != null)
+                return new ResponseEntity<Need>(needDao.updateNeed(need), HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Need> deleteNeed(@PathVariable int id) {
+        LOG.info("DELETE /needs/" + id);
+        try {
+            Need needToDelete = needDao.getNeed(id);
+            if (needToDelete != null)
+            {
+                needDao.deleteNeed(id);
+                return new ResponseEntity<Need>(HttpStatus.OK);
+            }
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
