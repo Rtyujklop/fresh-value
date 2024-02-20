a4ebf2f4616ac918c63bd18e174b003ee74ec6fa
Devin Rhodie <devinrhodie@gmail.com>
2024-02-20 18:58:18 -0500

    NeedDAO

--- ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/NeedDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/NeedDAO.java
@@ -1,3 +1,25 @@
-public class NeedDAO {
+package com.ufund.api.ufundapi.persistence;
+
+import java.io.IOException;
+
+import com.ufund.api.ufundapi.model.Need;
+
+public interface NeedDAO {
     
+    Need[] getNeeds() throws IOException;
+
+    
+    Need[] findNeeds(String containsText) throws IOException;
+
+    
+    Need getNeed(int id) throws IOException;
+
+   
+    Need createNeed(Need need) throws IOException;
+
+    
+    Need updateNeed(Need need) throws IOException;
+
+    
+    boolean deleteNeed(int id) throws IOException;
 }
