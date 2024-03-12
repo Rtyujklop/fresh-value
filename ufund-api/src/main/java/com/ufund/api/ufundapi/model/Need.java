e57f950b043a3193eb319b2227d477acda659d4d
Zach Herring <zdh7549@g.rit.edu>
2024-03-12 13:08:45 -0400

    Changed stuff to merge with main

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
@@ -17,7 +17,7 @@ public class Need {
     @JsonProperty("cost") private int cost;
     @JsonProperty("description") private String description;
 
-    public static final String STRING_FORMAT = "Need [id=%d, name=%s, cost=%d, description=%s]";
+    static final String STRING_FORMAT = "Need [id=%d, name=%s, cost=%d, description=%s]";
     /* 
      * Create a need with a given name, cost, description
      * @param id ID of need
