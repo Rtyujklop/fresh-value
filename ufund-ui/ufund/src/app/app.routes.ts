99458a9e51874213b968ee40e9d9b8e198fe3698
Ryan Noyes <rgn1061@rit.edu>
2024-03-20 06:05:37 -0400

    extremely basic ui, log-in does not work yet

--- ufund-ui/ufund/src/app/app.routes.ts
+++ ufund-ui/ufund/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { NeedsComponent } from './needs/needs.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'needs',
+        component: NeedsComponent,
+        title: 'Needs Page'
+    },
+    {
+        path:'',
+        component: UserLoginComponent,
+        title: 'Login'
+    }
+];
+
