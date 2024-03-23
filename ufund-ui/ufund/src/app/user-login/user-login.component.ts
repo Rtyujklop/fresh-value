ac9d00119c721160dc09a344c031823d469ca465
Devin Rhodie <devinrhodie@gmail.com>
2024-03-23 17:23:57 -0400

    added to user-login.component.ts

--- ufund-ui/ufund/src/app/user-login/user-login.component.ts
+++ ufund-ui/ufund/src/app/user-login/user-login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
-
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-user-login',
   standalone: true,
@@ -9,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './user-login.component.css'
 })
 export class UserLoginComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  login(credentials: any): void {
+    this.authService.login(credentials).subscribe(
+      () => {
+        // Redirect to the 'needs' route upon successful login
+        this.router.navigate(['/needs']);
+      },
+      (error) => {
+        // Handle login error if needed
+        console.error('Login error:', error);
+      }
+    );
+  }
 }
