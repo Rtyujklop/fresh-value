c45510bcd7a9eb196e3902d721eb1d10251882bc
Zach Herring <zdh7549@g.rit.edu>
2024-03-24 15:23:58 -0400

    changed made to login-view component

--- ufund-ui/u-fund/src/app/login-view/login-view.component.ts
+++ ufund-ui/u-fund/src/app/login-view/login-view.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
+import { Need } from '../need';
+import { NeedService } from '../need.service';
+import { User } from '../user';
+import { UserService } from '../user.service';
+import { MessageService } from '../message.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
+//import * as shajs from 'sha.js';
 @Component({
   selector: 'app-login-view',
   standalone: true,
@@ -8,5 +15,42 @@ import { Component } from '@angular/core';
   styleUrl: './login-view.component.css'
 })
 export class LoginViewComponent {
-
+  needs: Need[] = [];
+  log: string = "";
+  constructor(
+    private needService: NeedService,
+    private userService: UserService,
+    private messageService: MessageService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+  ngOnInit(): void {
+    this.getUsers();
+  }
+  switchPw(){
+    const el = document.getElementById("password");
+    if (el != null){
+      if(el.getAttribute("type") != "text"){
+        el.setAttribute("type", "password");
+      }
+    }
+  }
+  login(name: string, password: string){
+    this.getUsers();
+    this.setLog("Logging in...");
+    this.users.forEach(user => {
+      if (user.name == name){
+        let pass2 = shajs("shag256").update(string(name + password)).digest('hex');
+        if (user.password == pass2){
+          this.userService.setName(user.name);
+          this.setLog("successful login");
+        } else{
+          this.setLog("Invaild passowrd");
+        }
+      }
+    });
+    if (this.log == "Logging in ..."){
+      this.setLog("Username not found");
+    }
+  }
 }
--- ufund-ui/u-fund/src/app/user.service.ts
+++ ufund-ui/u-fund/src/app/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Need } from '../need';
-//import { User } from './user';
+import { User } from './user';
 import { HttpClient,HttpHeaders } from "@angular/common/http";
 import { Observable, of } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
@@ -14,7 +14,6 @@ export class UserService{
 
     constructor(
         private http: HttpClient,
-        //private httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' })},
         private messageService: MessageService) { }
 
     setName(name:string | null) {
@@ -45,6 +44,13 @@ export class UserService{
             tap(_ => this.log(`fetched user name =${name}`)),
             catchError(this.handleError<Need>(`getUser = ${name}`))
             );
+    }
+
+    addUser(user: User): Observable<User> {
+        return this.http.post<User>(this.usersUrl, user, this.httpOptions).pipe(
+            tap((newUser: User) => this.log(`added user with id=${newUser.name}`)),
+            catchError(this.handleError<User>('addUser'))
+        )
     }
 
     deleteUser(name: string): Observable<Need>{
