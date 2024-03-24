51ba3aa3dd31d6ff7146d2f6dc3f000ba8413781
Devin Rhodie <devinrhodie@gmail.com>
2024-03-24 19:03:41 -0400

    updated the login view now it's time for the html part of it.

--- ufund-ui/u-fund/src/app/app.routes.ts
+++ ufund-ui/u-fund/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { NeedsComponent } from './needs/needs.component';
 import { LoginViewComponent } from './login-view/login-view.component';
+import { FormsModule } from '@angular/forms';
 
 export const routes: Routes = 
 [
--- ufund-ui/u-fund/src/app/login-view/login-view.component.ts
+++ ufund-ui/u-fund/src/app/login-view/login-view.component.ts
@@ -1,71 +1,40 @@
 import { Component } from '@angular/core';
-import { Need } from '../need';
-import { NeedService } from '../need.service';
-import { User } from '../user';
 import { UserService } from '../user.service';
-import { MessageService } from '../message.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import shajs from 'sha.js';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-view',
-  standalone: true,
-  imports: [],
   templateUrl: './login-view.component.html',
-  styleUrl: './login-view.component.css'
+  styleUrls: ['./login-view.component.css']
 })
-
 export class LoginViewComponent {
-  users: User[] = [];
+  username: string = '';
+  password: string = '';
   log: string = "";
+
   constructor(
     private userService: UserService,
-    private messageService: MessageService,
-    private route: ActivatedRoute,
     private router: Router
   ) { }
 
-  ngOnInit(): void {
-    this.userService.getUsers();
-  }
-
-  switchPw(){
-    const el = document.getElementById("password");
-    if (el != null){
-      if(el.getAttribute("type") != "text"){
-        el.setAttribute("type", "password");
-      }
-    }
-  }
-
-  login(name: string, password: string){
-    this.userService.getUsers();
-    this.setLog("Logging in...");
-    this.users.forEach(user => {
-      if (user.name == name){
-        let pass2 = shajs("sha256").update(String(name + password)).digest('hex');
-        if (user.password == pass2){
-          this.userService.setName(user.name);
-          if (this.userService.getName() == "Admin") {
-            this.router.navigate(['../needs'], { relativeTo: this.route});
-          }
-          else {
-            this.router.navigate(['../userview'], { relativeTo: this.route});
-          }
-          this.setLog("successful login");
-        } 
-        else {
-          this.setLog("Invaild passowrd");
+  login() {
+    this.userService.getUsers().subscribe(users => {
+      const foundUser = users.find(user => user.name === this.username && user.password === this.password);
+      if (foundUser) {
+        this.userService.setName(foundUser.name);
+        if (foundUser.name === "Admin") {
+          this.router.navigate(['../needs']);
+        } else {
+          this.router.navigate(['../userview']);
         }
+        this.setLog("successful login");
+      } else {
+        this.setLog("Invalid username or password");
       }
     });
-    if (this.log == "Logging in ...") {
-      this.setLog("Username not found");
-    }
   }
 
   private setLog(message: string): void {
     this.log = message;
-    this.messageService.add(`LoginViewComponent: ${message}`);
   }
 }
